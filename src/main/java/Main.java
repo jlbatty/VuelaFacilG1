dfdaf935a925573ee3b92d9332f5f4aa032ec50f
julian <julian11992@hotmail.com>
2021-09-09 18:43:57 -0500

    C1: Carga prueba 32

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,8 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("holamundo");
+        System.out.println("PruebaPrueba");
+        System.err.println("eee");
     }
     
 }
