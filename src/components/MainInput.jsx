0207b9cb06e5a411f5db945fd8dbd89212125a38
Johnatan Andrés Arango R <ingyotis@gmail.com>
2021-09-11 18:02:59 -0500

    0.7 - Added an input test component. Testing useState

--- src/components/MainInput.jsx
+++ src/components/MainInput.jsx
@@ -1,5 +1,5 @@
 //libraries
-import {React, useState } from "react";
+import React, { useState } from "react";
 //styles
 import '../assets/styles/MainInput.css'
 
@@ -11,20 +11,37 @@ const MainInput = () => {
     //Esta función se encarga de cambiar el estado
     //con un condicional miro a qué tab le di click
     //también debe cambiar la clase del componente seleccionado, para que cambie su color
+    console.log(e.target.id)
+    if (e.target.id === 'vuelos'){
+      setSelected('vuelos')
+    } else if (e.target.id === 'checkin') {
+      setSelected('checkin')
+    } else if (e.target.id === 'reserva'){
+      setSelected('reserva')
+    }
+
   }
 
   return (
     <div className="mainInput">
       <div className="tabs-container">
-        <div className="mainInput__tab isSelected" onClick={handleClick}>Vuelos</div>
-        <div className="mainInput__tab">Check-in</div>
-        <div className="mainInput__tab">Mi Reserva</div>
+        <div id="vuelos" className="mainInput__tab isSelected" onClick={handleClick}>Vuelos</div>
+        <div id="checkin" className="mainInput__tab" onClick={handleClick}>Check-in</div>
+        <div id="reserva" className="mainInput__tab" onClick={handleClick}>Mi Reserva</div>
       </div>
       <div className="input-container">
         {
           selected === 'vuelos' && 
           <h2>Acá debería ir el componente input para los vuelos</h2>
         }
+        {
+          selected === 'checkin' &&
+          <h2>Acá debería ir el componente para hacer el check in </h2>
+        }
+        {
+          selected === 'reserva' &&
+          <h2>Acá debería ir el componente para hacer buscar una reserva </h2>
+}
       </div>
     </div>
   )
